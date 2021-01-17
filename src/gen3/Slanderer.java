6130f9739a1c345cec0d37951a6dc91ab80c94c1
Himanshu <himanshukumar1125@gmail.com>
2021-01-18 03:25:08 +0530

    implemented fixed-radius slanderer wall

--- src/gen3/Slanderer.java
+++ src/gen3/Slanderer.java
@@ -1,13 +1,70 @@
 package gen3;
 
-import battlecode.common.*;
-
-import static gen3.helpers.MovementHelper.getRandomDirection;
-import static gen3.helpers.MovementHelper.tryMove;
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.RobotInfo;
+import static gen3.RobotPlayer.*;
 
 public strictfp class Slanderer {
+	private static final int WALL_RADIUS_MIN = 18;
+	private static final int WALL_RADIUS_MAX = 30;
+
+	private static Direction toMove;
+	private static double maxPassability;
 
     public static void move() throws GameActionException {
-        tryMove(getRandomDirection());
+        if (!rc.isReady()) {
+        	return;
+        }
+
+        int distanceFromSpawner = rc.getLocation().distanceSquaredTo(spawnerLocation);
+        System.out.println(distanceFromSpawner);
+        if (distanceFromSpawner >= WALL_RADIUS_MIN && distanceFromSpawner <= WALL_RADIUS_MAX) {
+        	return;
+        }
+
+        toMove = null;
+        maxPassability = 0d;
+
+        Direction straight = rc.getLocation().directionTo(spawnerLocation).opposite();
+        updateDirection(straight);
+        Direction left = straight.rotateLeft();
+        updateDirection(left);
+        Direction right = straight.rotateRight();
+        updateDirection(right);
+        if (maxPassability > 0 && tryMove(toMove)) {
+        	return;
+        }
+
+        for (int i = 0; i < 2; ++i) {
+        	left = left.rotateLeft();
+        	if (tryMove(left)) {
+        		return;
+        	}
+
+        	right = right.rotateRight();
+        	if (tryMove(right)) {
+        		return;
+        	}
+        }
+
+        left = left.rotateLeft();
+        tryMove(left);
+    }
+
+    private static void updateDirection(Direction dir) throws GameActionException {
+    	double passability = rc.sensePassability(rc.getLocation());
+    	if (passability > maxPassability && rc.canMove(dir)) {
+    		toMove = dir;
+    		maxPassability = passability;
+    	}
+    }
+
+    private static boolean tryMove(Direction dir) throws GameActionException {
+    	if (rc.canMove(dir)) {
+    		rc.move(dir);
+    		return true;
+    	}
+    	return false;
     }
 }
