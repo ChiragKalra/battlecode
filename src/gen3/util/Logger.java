5469213bd57e981c66625b50d41afcfbc51e9d4c
IshanKumarKaler <54807073+ishankumarkaler@users.noreply.github.com>
2021-01-17 23:21:01 +0530

    Update gen3 logger

--- src/gen3/util/Logger.java
+++ src/gen3/util/Logger.java
@@ -8,15 +8,18 @@ public class Logger {
     private final int start, startRN;
     private int prev, prevRN;
     private final StringBuilder output = new StringBuilder();
+    private int totalLogs;
 
-    public Logger (String title) {
+    // LogCounts is total number of logs for a class including total.
+    public Logger (String title, int logCounts) {
+        totalLogs = logCounts;
         start = prev = Clock.getBytecodeNum();
         startRN = prevRN = rc.getRoundNum();
-        output.append(title).append(" Log Start@").append(prevRN).append('\n');
+        output.append("Log_Start@@").append(prevRN).append(" ").append(totalLogs).append(" ").append(title).append('\n');
     }
 
     public void log (String event) {
-        output.append(event).append('=')
+        output.append(event).append(" ")
                 .append(Clock.getBytecodeNum()-prev + (rc.getRoundNum()-prevRN)*mType.bytecodeLimit).append('\n');
         prev = Clock.getBytecodeNum();
         prevRN = rc.getRoundNum();
@@ -25,7 +28,7 @@ public class Logger {
     public void flush () {
         if (DEBUG) {
             log("end");
-            output.append("-----total=")
+            output.append("total ")
                     .append(Clock.getBytecodeNum()-start + (rc.getRoundNum()-startRN)*mType.bytecodeLimit);
             System.out.println(output.toString());
         }
