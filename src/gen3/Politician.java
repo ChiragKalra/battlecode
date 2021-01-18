91ea427707e02c52c6dd67951ec46d3a92dfb757
Himanshu <himanshukumar1125@gmail.com>
2021-01-19 01:43:44 +0530

    created politician wall

--- src/gen3/Politician.java
+++ src/gen3/Politician.java
@@ -2,19 +2,23 @@ package gen3;
 
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
+import battlecode.common.Direction;
+import battlecode.common.RobotController;
 import gen3.util.Pair;
 import gen3.util.SpawnType;
 
 import static gen3.RobotPlayer.rc;
 import static gen3.helpers.AttackHelper.*;
 import static gen3.helpers.MovementHelper.*;
-
+import static gen3.flags.EnlightenmentCenterFlag.getRadius;
+import static gen3.RobotPlayer.*;
+import static gen3.helpers.DefenseHelper.*;
 
 public strictfp class Politician {
     public static boolean isAttackType = false;
 
     public static void init() {
-        isAttackType = rc.getInfluence() > SpawnType.DefensePolitician.maxHp;
+        isAttackType = rc.getInfluence() % 2 == 1;
     }
 
     private static MapLocation locToEmp = null;
@@ -41,14 +45,55 @@ public strictfp class Politician {
         }
     }
 
+    public static int innerRadius, outerRadius;
+    public static MapLocation[] innerPoints, outerPoints;
+
     private static void moveDefense () throws GameActionException {
         int rad = shouldAttackDefensive();
         if (rad != 0) {
             rc.empower(rad);
-        } else {
-            // TODO Keep wall
-            tryMove(getRandomDirection());
+            return;
         }
+
+        int radius = getRadius(rc.getFlag(enlightenmentCenterId));
+        innerRadius = radius * radius + 1;
+        outerRadius = (radius + 1) * (radius + 1) + 1;
+
+        innerPoints = getCircumferencePoints(spawnerLocation, innerRadius);
+        outerPoints = getCircumferencePoints(spawnerLocation, outerRadius);
+
+        if (onWall(rc.getLocation()) && !isTunnelPoint(rc.getLocation())) {
+            return;
+        }
+
+        Direction straight = rc.getLocation().directionTo(spawnerLocation).opposite();
+        Direction left = straight.rotateLeft();
+        Direction right = straight.rotateRight();
+
+        if (tryMoveWall(straight)) {
+            return;
+        }
+        if (tryMoveWall(left)) {
+            return;
+        }
+        if (tryMoveWall(right)) {
+            return;
+        }
+
+        for (int i = 0; i < 2; ++i) {
+            left = left.rotateLeft();
+            if (tryMoveWall(left)) {
+                return;
+            }
+
+            right = right.rotateRight();
+            if (tryMoveWall(right)) {
+                return;
+            }
+        }
+
+        left = left.rotateLeft();
+        tryMoveWall(left);
     }
 
     public static void move() throws GameActionException {
