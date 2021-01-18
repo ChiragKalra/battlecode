cdabe3e407d4ed535328494965bb0ab6d7041709
Himanshu <himanshukumar1125@gmail.com>
2021-01-19 03:19:36 +0530

    created slanderer wall

--- src/gen3/Slanderer.java
+++ src/gen3/Slanderer.java
@@ -2,14 +2,19 @@ package gen3;
 
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+
 import static gen3.RobotPlayer.*;
+import static gen3.flags.EnlightenmentCenterFlag.getRadius;
+import static gen3.helpers.DefenseHelper.*;
+import static gen3.helpers.MovementHelper.getCircumferencePoints;
 
 public strictfp class Slanderer {
 	private static final int WALL_RADIUS_MIN = 18;
 	private static final int WALL_RADIUS_MAX = 30;
 
-	private static Direction toMove;
-	private static double maxPassability;
+	public static int innerRadius, outerRadius;
+    public static MapLocation[] innerPoints, outerPoints;
 
     public static void move() throws GameActionException {
         if (!rc.isReady()) {
@@ -19,49 +24,75 @@ public strictfp class Slanderer {
         if (spawnerLocation == null) {
             return;
         }
-        int distanceFromSpawner = rc.getLocation().distanceSquaredTo(spawnerLocation);
-        if (distanceFromSpawner >= WALL_RADIUS_MIN && distanceFromSpawner <= WALL_RADIUS_MAX) {
-        	return;
-        }
 
-        toMove = null;
-        maxPassability = 0d;
+        int radius;
+        if (rc.canGetFlag(enlightenmentCenterId)) {
+            radius = getRadius(rc.getFlag(enlightenmentCenterId));
+        } else {
+            return;
+        }
+        
+        innerRadius = radius * radius + 1;
+        outerRadius = (radius + 1) * (radius + 1) + 1;
+
+        innerPoints = getCircumferencePoints(spawnerLocation, innerRadius);
+        outerPoints = getCircumferencePoints(spawnerLocation, outerRadius);
 
         Direction straight = rc.getLocation().directionTo(spawnerLocation).opposite();
-        updateDirection(straight);
         Direction left = straight.rotateLeft();
-        updateDirection(left);
         Direction right = straight.rotateRight();
-        updateDirection(right);
-        if (maxPassability > 0 && tryMove(toMove)) {
-        	return;
+
+        if (tryMoveSlanderer(straight)) {
+            return;
+        }
+        if (tryMoveSlanderer(left)) {
+            return;
+        }
+        if (tryMoveSlanderer(right)) {
+            return;
+        }
+
+        boolean nearWall = false;
+        if (onWall(rc.getLocation().add(straight)) || onWall(rc.getLocation().add(left)) || 
+            onWall(rc.getLocation().add(right))) {
+            nearWall = true;
+        }
+
+        if (nearWall) {
+            if (isTunnelPoint(rc.getLocation())) {
+                for (int i = 0; i < 2; ++i) {
+                    left = left.rotateLeft();
+                    if (tryMoveSlanderer(left)) {
+                        return;
+                    }
+
+                    right = right.rotateRight();
+                    if (tryMoveSlanderer(right)) {
+                        return;
+                    }
+                }
+            }
+
+            return;
         }
 
         for (int i = 0; i < 2; ++i) {
-        	left = left.rotateLeft();
-        	if (tryMove(left)) {
-        		return;
-        	}
+            left = left.rotateLeft();
+            if (tryMoveSlanderer(left)) {
+                return;
+            }
 
-        	right = right.rotateRight();
-        	if (tryMove(right)) {
-        		return;
-        	}
+            right = right.rotateRight();
+            if (tryMoveSlanderer(right)) {
+                return;
+            }
         }
-
-        left = left.rotateLeft();
-        tryMove(left);
     }
 
-    private static void updateDirection(Direction dir) throws GameActionException {
-    	double passability = rc.sensePassability(rc.getLocation());
-    	if (passability > maxPassability && rc.canMove(dir)) {
-    		toMove = dir;
-    		maxPassability = passability;
-    	}
-    }
-
-    private static boolean tryMove(Direction dir) throws GameActionException {
+    private static boolean tryMoveSlanderer(Direction dir) throws GameActionException {
+        if (onWall(rc.getLocation().add(dir))) {
+            return false;
+        }
     	if (rc.canMove(dir)) {
     		rc.move(dir);
     		return true;
